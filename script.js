466adb6c431d231d08ec55a72b33969f168f9e86
loganmcbride148 <loganmcbride148@gmail.com>
2019-10-24 21:48:42 +0000

    Got basic functionality to work, could add some style and minor tweaks.

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 let app = new Vue({
     el: '#app',
     data: {
-        searchInput: ''
+        searchInput: '',
+        rows: [],
+        columns: ['Title', 'Work', 'Artist']
     },
     methods: {
         async search() {
-            const response = await axios.get("https://www.rijksmuseum.nl/api/en/collection?q=Q&key=btZpg4ag&format=json".replace("Q", searchInput));
-            
-            
+            this.rows = [];
+            const response = await axios.get("https://www.rijksmuseum.nl/api/en/collection?q=Q&key=btZpg4ag&format=json".replace("Q", this.searchInput));
+            for(let i = 0; i < response.data.artObjects.length; i++) {
+                let title = '';
+                let work = '';
+                let artist = '';
+                
+                if(response.data.artObjects[i].title != '') {
+                    title = response.data.artObjects[i].title;
+                } else {
+                    title = "Unknown Title";
+                }
+                if(response.data.artObjects[i].hasImage === false) {
+                    work = '';
+                } else {
+                    work = response.data.artObjects[i].webImage.url.replace("s0", "s256");
+                }
+                if(response.data.artObjects[i].artist === "") {
+                    artist = "Unknown Artist";
+                } else {
+                    artist = response.data.artObjects[i].principalOrFirstMaker;
+                }
+                this.rows.push({title: title, work: work, artist: artist});
+                
+            }
         }
     }
 })
